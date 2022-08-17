7383f42e9e090b12eb3ca78de69bed784ba46dbc
Joao <joao@houzing.eu>
2022-08-17 17:21:08 +0100

    Initial Commit

---
+++ Receipt List/Receipt_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Receipt_ListApp.swift
+//  Receipt List
+//
+//  Created by Joao Pedro Pereira on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Receipt_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
